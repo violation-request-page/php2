6b68cccc5a65044767dcaa727e69faf79476d9dd
violation-request-page <145922328+violation-request-page@users.noreply.github.com>
2026-07-08 07:35:40 +0500

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $mail->Host       = 'smtp-relay.brevo.com';
         $mail->SMTPAuth   = true;
         $mail->Username   = 'ab2b68001@smtp-brevo.com'; 
-        $mail->Password   = 'bsknvfTPYzK3DVt'; 
+        $mail->Password   = 'bskP1kjH3tN0gBK'; 
         $mail->Port       = 587;
         $mail->SMTPSecure = 'tls'; 
 
